1215ac032be4ad393725509fcd85a7e6910b8718
Jimmy Silva <jimmysilva@Foundation-28s-MacBook-Air.local>
2019-05-21 15:56:45 -0300

    possivelinstalacaobotao

--- src/app/telasCapsula/categorias-capsula/categorias-capsula.component.ts
+++ src/app/telasCapsula/categorias-capsula/categorias-capsula.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-categorias-capsula',
   templateUrl: './categorias-capsula.component.html',
@@ -13,3 +14,32 @@ export class CategoriasCapsulaComponent implements OnInit {
   }
 
 }
+
+
+import {MatDialog} from '@angular/material';
+
+/**
+ * @title Dialog with header, scrollable content and actions
+ */
+@Component({
+  selector: 'dialog-content-example',
+  templateUrl: 'dialog-content-example.html',
+  styleUrls: ['dialog-content-example.css'],
+})
+export class DialogContentExample {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogContentExampleDialog);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-content-example-dialog',
+  templateUrl: 'dialog-content-example-dialog.html',
+})
+export class DialogContentExampleDialog {}
